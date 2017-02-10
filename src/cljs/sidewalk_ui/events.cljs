781e2055c6ff5e91a78d4609158a426e1c9f60ea
Paul <paulll.lucas@gmail.com>
2017-02-10 00:10:35 -0800

    Broken state

--- src/cljs/sidewalk_ui/events.cljs
+++ src/cljs/sidewalk_ui/events.cljs
@@ -15,7 +15,13 @@
 (re-frame/reg-event-db
  :set-pixel
  (fn [db [_ pixel]]
-   (assoc (:pixel-vec db) :key pixel)))
+   ;; (assoc (:pixel-vec db) :key pixel)
+   ;; (js/console.log (:pixel-vec db))
+   ;; (js/console.log  (:key ((:pixel-vec db) (:key pixel))))
+   ;; (js/console.log pixel)
+   (assoc (:pixel-vec db)
+          (:key ((:pixel-vec db) (:key pixel)))
+          pixel)))
 
 (re-frame/reg-event-db
  :set-active-color
